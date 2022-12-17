250cc6eea788aa1f4fd6bfc13b7b1d3eefe62233
Mariya Lagun <108588991+mmarryll@users.noreply.github.com>
2022-12-17 22:54:45 +0300

    Add files via upload

---
+++ Main/BananaAndCocoaMilkshake.java
@@ -0,0 +1,12 @@
+//Молочный коктейль 1.0 с бананами и какао
+public class BananaAndCocoaMilkshake extends Milkshake {
+
+    public void addFruits() {
+        System.out.println("Banana has been added.");
+    }
+
+    public void addSweeties() {
+        System.out.println("Cocoa has been added.");
+    }
+
+}
---
+++ Main/StrawberryAndCookiesMilkshake.java
@@ -0,0 +1,11 @@
+//Молочный коктейль 2.0 с клубникой и печеньем
+public class StrawberryAndCookiesMilkshake extends Milkshake {
+
+    public void addFruits() {
+        System.out.println("Strawberry has been added.");
+    }
+
+    public void addSweeties() {
+        System.out.println("Cookies have been added.");
+    }
+}
